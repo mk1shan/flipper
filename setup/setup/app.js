58717b406a5f85b4972ab99cb39d4af05fa405b4
Mudipa kishan imayanga <128371632+mk1shan@users.noreply.github.com>
2023-12-05 01:06:52 +0530

    Update app.js

--- setup/setup/app.js
+++ setup/setup/app.js
@@ -6,7 +6,7 @@ const color =document.querySelector(".color");
 btn.addEventListener('click',function(){
     //get random num between 0-3 
 
-    const randomNumber =2;
+    const randomNumber =
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent =colors[randomNumber];
     
